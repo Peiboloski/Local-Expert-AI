1167fa4c5664f22494964a576e45dbb05fbe410e
Pablo D <padelgar@gmail.com>
2024-03-21 12:17:30 +0100

    feat: change landing page and include screenshots of the app

--- app/_components/molecules/header/index.tsx
+++ app/_components/molecules/header/index.tsx
@@ -1,8 +1,12 @@
+'use client'
 import Link from "next/link";
 import { ButtonLink } from "../../atoms/button";
+import { usePathname } from 'next/navigation'
 
 // Header component
 function Header() {
+    const pathname = usePathname()
+    const isHomePage = pathname === '/'
     return (
         <header className="flex flex-row gap-ds-12 justify-between self-stretch px-ds-32 max-[400px]:px-ds-24 py-ds-16 bg-ds-grey-900">
             <nav className="flex flex-row w-[100%] self-stretch gap-ds-12 justify-between align-middle">
@@ -10,9 +14,9 @@ function Header() {
                     <span className="text-ds-grey-100 text-ds-16 font-medium">LOCAL EXPERT</span>
                     <span className="text-ds-green-500 text-ds-16 font-medium"> AI</span>
                 </Link>
-                <ButtonLink type="primary" size="small" href="/explore">
-                    Explore nearby atractions
-                </ButtonLink>
+                {isHomePage && <ButtonLink type="primary" size="small" href="/explore">
+                    Start Exploring
+                </ButtonLink>}
             </nav>
         </header>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { ButtonLink } from "./_components/atoms/button";
 import Section from "./_components/atoms/section";
 
@@ -25,21 +26,28 @@ function Hero() {
 
 function FeatureDescription() {
   return (
-    <Section className="flex flex-col py-ds-16 gap-ds-24">
+    <Section className="flex flex-col py-ds-16 gap-ds-24 pb-ds-64">
       <header className="txt-section-label whitespace-nowrap">
         HOW IT WORKS?
       </header>
-      <p className="txt-main-text-medium">1. Select the nearby landmark you are currently visiting</p>
-      <p className="txt-main-text-medium">2. Check the summary with the most important information of the place</p>
-      <p className="txt-main-text-medium">3. Chat with our local guide AI and ask anything else you want to know</p>
+      <p className="txt-main-text-medium">1. Enable GPS location and get the key information of the place you are currently visiting.</p>
+      <Image src="/images/HomePageMockup.png" width={400} height={1000} alt="Home page mockup" />
+      <p className="txt-main-text-medium">2. Check the list with the musts during your visit, and the traditional food of the city.</p>
+      <Image src="/images/mustVisitMockup.png" width={400} height={1000} alt="Explore page mockup" />
+      <p className="txt-main-text-medium">3. Chat with our local guide AI to get the key information of a particular place, and ask anything else you want to know.</p>
+      <Image src="/images/chatMockup.png" width={400} height={1000} alt="Chat page mockup" />
     </Section>
   );
 }
 
 function ExploreNearbyAttractions() {
   return (
-    <Section className="flex flex-col self-stretch text-base font-medium max-w-[430px] text-neutral-700 py-ds-32 pb-ds-64">
-      <ButtonLink href="/explore" type="secondary" size="normal">Explore nearby attractions</ButtonLink>
+    <Section wrapperClassName="bg-ds-grey-800" className="flex flex-col justify-center items-center self-stretch text-base font-medium max-w-[430px] text-neutral-700 py-ds-64 gap-ds-32">
+      <p className="txt-hero text-ds-grey-100 mx-auto">Try it for <span className="text-ds-green-500">free</span></p>
+      <div className="flex justify-center">
+        <ButtonLink href="/explore" type="secondary" size="normal">Start exploring</ButtonLink>
+      </div>
+
     </Section>
   );
 }
